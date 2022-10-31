91f1541b2b770196856ee0c9d3d024d3f4679ee7
abeni3 <abenitamene2023@u.northwestern.edu>
2022-10-31 03:11:07 -0500

    chaging BFS

--- src/BFS.py
+++ src/BFS.py
@@ -1,11 +1,13 @@
+from platform import node
+from turtle import clear
 import pandas as pd
 import sys
 import os
+import random
+import copy
 
 current = os.path.dirname(os.path.realpath(__file__))
-
 parent = os.path.dirname(current)
-
 sys.path.append(parent)
 
 import utils
@@ -18,21 +20,16 @@ mgraph = utils.create_network(df, vis_attributes, 50, missing)
 
 neighbor_info = utils.get_neighbor_information() 
 
-constraint = {"top": 'age>28', "gender": 1}
+#test constrains
+top = {'public': '1'}
+med = {'gender': '0'}
+bot = {'bot1': '180', 'gender':'male'}
 
-'''
-top constraint: age>=28
-medium constraint: gender = 1
-'''
-
-def BFS(graph, source_node, constraint, size=50):
+def BFS(graph, source_node, constraint_dict, size=50):
 
     # Mark all the vertices as not visited
     visited = [False] * (size)
-
-    # Create a queue for BFS
     queue = []
-
     # Mark the source node as
     # visited and enqueue it
     queue.append(source_node)
@@ -41,11 +38,9 @@ def BFS(graph, source_node, constraint, size=50):
     while queue:
 
         current = queue.pop(0)
-        if(constraint == 'top'):
-            if (graph.nodes[current]['AGE'] >= 28):
-                result.append(current)
-        if(constraint == 'med'):
-            if (graph.nodes[current]['gender'] == 1):
+        if( 'public' in constraint_dict.keys()):
+   
+            if (graph.nodes[current]['public'] == int(constraint_dict['public'])):
                 result.append(current)
 
         # Get all adjacent vertices of the
@@ -55,12 +50,7 @@ def BFS(graph, source_node, constraint, size=50):
                 queue.append(i)
                 visited[i - 1] = True 
     return result
-top_arr = BFS(mgraph, mgraph.nodes[32]['user_id'], constraint='top')
-print(top_arr[0])
-med_graph = mgraph.subgraph(top_arr)
-# print(med_graph.nodes[36])
-med_arr = BFS(med_graph, med_graph.nodes[top_arr[0]]['user_id'], constraint='med')
-print(med_arr)
 
-
+top_constraint_qualifers = BFS(mgraph, mgraph.nodes[1]['user_id'], constraint_dict=top)
+print(top_constraint_qualifers)
 
