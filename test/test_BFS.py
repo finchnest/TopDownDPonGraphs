33db49b4dc3e71d62ba03e75c855d3b1166e6e52
abeni3 <abenitamene2023@u.northwestern.edu>
2022-11-13 21:03:53 -0600

    bug fix

--- test/test_BFS.py
+++ test/test_BFS.py
@@ -2,7 +2,10 @@ import sys
 import unittest
 
 from pathlib import Path
-sys.path.insert(0, str(Path(__file__).resolve().parent.parent / 'src'))
+import os
+current = os.path.dirname(os.path.realpath(__file__))
+parent = os.path.dirname(current)
+sys.path.append(parent)
 
 import DP
 import BFS
@@ -13,8 +16,8 @@ class test_BFS(unittest.TestCase):
     def testPreBfs(self):
         appArgs = DP.parseArgs(['-t', 'region_large=Nitra Region', '-m', 'region_small=Nitra', '-b', 'age>=32'])
         arr = BFS.preBFS(appArgs)
-        self.assertEqual(arr[0][0], 14141)
-        self.assertEqual(arr[0][1], 15494)
+        self.assertEqual(arr[0][1], 14141)
+        self.assertEqual(arr[0][0], 15494)
         self.assertEqual(arr[0][2], 6139)
         self.assertEqual(arr[0][3], 14889)
         self.assertEqual(arr[0][4], 15872)
